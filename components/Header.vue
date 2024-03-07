a304bec8ddf4c1e7ca34b3b5bafba6f1e36695e7
lixuan <1318753541@qq.com>
2024-03-07 11:10:55 +0800

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,13 @@
 <template>
   <header class="bg-white">
     <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-      <div class="flex h-16 items-center justify-between">
-        <div class="hidden md:block">
-          <nav aria-label="Global">
-            <ul class="flex items-center gap-6 text-sm">
-              <li v-for="dynasty in dynasties">
-                <a
-                  class="text-gray-500 transition hover:text-gray-500/75"
-                  href="#">
-                  {{ dynasty }}
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div class="flex h-16 items-center justify-center gap-8">
+        <a
+          v-for="dynasty in dynasties"
+          class="text-gray-500 transition hover:text-gray-500/75"
+          href="#">
+          {{ dynasty }}
+        </a>
       </div>
     </div>
   </header>
@@ -22,6 +15,12 @@
 
 <script lang="ts" setup>
 const dynasties = ['先秦', '秦', '汉', '唐', '宋', '元', '明', '清', '其他']
+
+const asyncData = async () => {}
+
+onMounted(() => {
+  asyncData()
+})
 </script>
 
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">白日依山尽</h1>
+    <h1 class="text-3xl">白日依山尽</h1>
   </div>
 </template>
 
